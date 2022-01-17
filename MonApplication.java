a786e557a77456fdb85106e05aa7b34ab9f1f884
Nana Kone <nana.fatoumata.kone@gmail.com>
2022-01-17 23:49:15 +0000

    maybe this one

---
+++ MonApplication.java
@@ -0,0 +1,6 @@
+public class MonApplication{
+    public static void main (String arg[]) {
+        System.out.println("Bienvenue dans mon application de gestion de ressources humaines");
+        
+    }
+}
