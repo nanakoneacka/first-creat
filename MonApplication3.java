46298e61ecaaba3e1babafbbcee67c681c44dff8
nanakoneacka <97602738+nanakoneacka@users.noreply.github.com>
2022-01-18 01:24:11 +0000

    Add files via upload

---
+++ MonApplication3.java
@@ -0,0 +1,17 @@
+public class MonApplication3 {
+
+    public static void main (String arg[]) {
+        System.out.println("démo condition");
+
+        int i = 0;
+
+        if (i < 0)
+            System.out.println("le nombre est négatif");
+            
+        else if(i > 0)
+                System.out.println("ce nombre est positif");
+        else
+            System.out.println("le nombre est null");
+            
+    }
+}
